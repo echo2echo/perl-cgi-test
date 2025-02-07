b3d8d562038722241434ca3c4e04ff2a346f937a
Joel Rader <echophotographic@gmail.com>
2025-02-07 15:16:50 -0500

    Update perl-cgi-test.pl

--- perl-cgi-test.pl
+++ perl-cgi-test.pl
@@ -21,10 +21,8 @@ my $microseconds;
 
 ($seconds, $microseconds) = gettimeofday;
 
-### handle smaill numbers
-
+### handle small numbers ###
 $microseconds = $seconds * 1000 + $microseconds / 1000;
-
 
 ### End beginning of execution time.... See Below for final interval code
 
