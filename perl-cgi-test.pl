d795913648685254ea0186b945be8dd76484ef17
Joel Rader <echophotographic@gmail.com>
2025-02-07 23:12:01 -0500

    Update perl-cgi-test.pl

--- perl-cgi-test.pl
+++ perl-cgi-test.pl
@@ -3,7 +3,7 @@
 ##   Use the previous shbang line to use system perl
 
 ##   Use the following shbang if you have perlbrew on your system.  Change username below to your local username and your version of perl to use perlbrew in your cgi-script ... For use on linux.. Ubuntu Linux used by me
-##   #!/home/user/perl5/perlbrew/perls/perl-5.40.1/bin/perl
+##   #!/home/username/perl5/perlbrew/perls/perl-5.40.1/bin/perl
 
 use strict;
 use warnings;
