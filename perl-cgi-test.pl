b8a9271a632dbbdb9a2cce4af3276e49e867f871
Joel Rader <echophotographic@gmail.com>
2025-02-08 21:05:49 -0500

    Update perl-cgi-test.pl
    remove <br>u

--- perl-cgi-test.pl
+++ perl-cgi-test.pl
@@ -58,7 +58,7 @@ my $last_mod_time = (stat ($file))[9];
 my $timetoreadable = localtime($last_mod_time);
 
 # local time here is EST
-print "File last updated: $timetoreadable EST <br><br>\n";
+print "File last updated: $timetoreadable EST <br>\n";
 
 ### End file last modified ##
 
